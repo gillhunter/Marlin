1b54257d7d2132ae379489123ef91c5939445b59
gillhunter <51458956+gillhunter@users.noreply.github.com>
2020-07-31 15:12:06 -0230

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -982,7 +982,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -30, -20, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1031,7 +1031,7 @@
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
-#define Z_PROBE_OFFSET_RANGE_MIN -20
+#define Z_PROBE_OFFSET_RANGE_MIN 0
 #define Z_PROBE_OFFSET_RANGE_MAX 20
 
 // Enable the M48 repeatability test to test probe accuracy
